94929e972b4e906c28a842b0cd2242910da4b4c9
Joao Pedro <jps12carvalho@gmail.com>
2022-09-19 10:55:03 -0300

    feat: Configuring consumer

--- spark/consumer_lula.py
+++ spark/consumer_lula.py
@@ -10,29 +10,49 @@ from elasticsearch import Elasticsearch
 
 ELASTIC_SEARCH_HOST='http://es01:9200'
 KAFKA_HOST='kafka:9092'
+TOPIC='lula'
 
 time.sleep(60)
 
+words_black_list = {
+    "em",
+    "rt",
+    "do",
+    "da",
+    "de",
+    "e",
+    "o",
+    "a",
+    "que",
+    "com",
+    "no",
+    "na",
+    "para",
+    "um",
+    "tem",
+    "se",
+}
 
 es = Elasticsearch(ELASTIC_SEARCH_HOST)
 if __name__ == '__main__':
     # Kafka Consumer 
     consumer = KafkaConsumer(
-        'lula',
+        TOPIC,
         bootstrap_servers=KAFKA_HOST,
         #auto_offset_reset='earliest',
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
-
-
-
     
     for message in consumer:
     #    print(json.loads(message.value))
 
-       doc = {
-             "text": f"{message.value}",
-             "timestamp": datetime.now()}
-       
-       res = es.index(index="lula", body=doc)
-       print(res['result'])
+        for word in message.value.split(' '):
+            if word in words_black_list:
+                continue
+            doc = {
+                    "text": f"{word}",
+                    "timestamp": datetime.now()
+            }
+            
+            res = es.index(index=TOPIC, body=doc)
+            print(res['result'])
