0602a271cd5a1ccd20d101b141a9b0561314c0ea
thiagomesUNB <thiago.luiz20000@gmail.com>
2022-09-19 09:03:29 -0300

    Removendo dados vazios

--- spark/spark_producer.py
+++ spark/spark_producer.py
@@ -17,7 +17,7 @@ class TwitterProducer:
     def __init__(self):
         self.token = 'AAAAAAAAAAAAAAAAAAAAADNVhAEAAAAAfgohmt4oXJK3eN5WFjzxMpDIUKY%3DGVdP4Fd6Zgvpxz5IvMmnjasTP7cSDz3VsswUBUTjXtUmZHwM3J'
         self.time()
-        self.query_params = {'query': 'Lula OR Bolsonaro lang:pt ','max_results': '10','tweet.fields': 'created_at','start_time': self.start_time_str,'end_time': self.end_time_str}
+        self.query_params = {'query': 'Lula OR Bolsonaro lang:pt ','max_results': '100','tweet.fields': 'created_at','start_time': self.start_time_str,'end_time': self.end_time_str}
         self.my_producer = KafkaProducer(bootstrap_servers = [KAFKA_HOST],value_serializer = lambda x:json.dumps(x).encode('utf-8'))
         self.search_url = "https://api.twitter.com/2/tweets/search/recent"
         
@@ -73,11 +73,14 @@ class TwitterProducer:
 
             for i in range(len(self.spark_df)):
 
-                self.my_producer.send(value = self.spark_df['bolsonaro'][i],topic = 'bolsonaro')
+                if self.spark_df['bolsonaro'][i] != '' and self.spark_df['lula_bolsonaro'][i] == '':
+                    self.my_producer.send(value = self.spark_df['bolsonaro'][i],topic = 'bolsonaro')
 
-                self.my_producer.send(value = self.spark_df['lula'][i],topic = 'lula')
-
-                self.my_producer.send(value = self.spark_df['lula_bolsonaro'][i],topic = 'lula_bolsonaro')
+                if self.spark_df['lula'][i] != '' and self.spark_df['lula_bolsonaro'][i] == '':
+                    self.my_producer.send(value = self.spark_df['lula'][i],topic = 'lula')
+                
+                if self.spark_df['lula_bolsonaro'][i] != '':
+                    self.my_producer.send(value = self.spark_df['lula_bolsonaro'][i],topic = 'lula_bolsonaro')
             
             
             self.time(booleana = False)
